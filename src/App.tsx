81df91d18376aa5feec2fc56bbb85e389ba47ccb
Kqn0n <taharacomeon.0504@gmail.com>
2026-01-17 09:48:28 +0900

    refactor: stylesのリファクタリング & 外部リソース排除

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { AuthProvider } from './context/AuthContext';
 import { AppRouter } from './routes/AppRouter';
 import { ErrorBoundary } from './components/common/ErrorBoundary';
-import './App.css';
 
 const App: React.FC = () => {
   return (
